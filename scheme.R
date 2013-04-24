f51fece03e90e0bef1e714a5132920a6c4819af2
David Springate <daspringate@gmail.com>
2013-04-24 15:52:18 +0100

    Built InPort on S3 and closures

--- scheme.R
+++ scheme.R
@@ -1,7 +1,9 @@
 ## SchemeR - an implementation of scheme in R
 ## Copyright (c) David Springate 2013
 
-schemeR.version <- 2.02
+require(stringr)
+
+schemeR.version <- 3.01
 
 Env <- function(outer = new.env()){
   # defines a new environment
@@ -160,3 +162,101 @@ schemeR <- function(prompt = "schemeR>> "){
 
 global.env <- Env()
 add.globals(global.env)
+
+####
+
+Symbol <- function(x=NULL) {
+    structure(x, class="Symbol")
+}
+
+
+sym <- function(x, symbol.table = new.env()){
+     # Find or create unique Symbol entry for str s in symbol table.
+    if(!x %in% ls(symbol.table)) assign(paste0(x, "_"), Symbol(x), envir = symbol.table)
+    symbol.table
+}
+
+stab = new.env()
+
+symbol.list <- "quote   if   set!  define   lambda   begin   define-macro quasiquote  unquote   unquote-splicing"
+for(s in unlist(strsplit(symbol.list, split="[ \n]+"))){
+    sym(s, stab)
+}
+
+
+eof.object = Symbol('#<eof-object>') 
+
+
+
+def parse(inport):
+    "Parse a program: read and expand/error-check it."
+# Backwards compatibility: given a str, convert it to an InPort
+if isinstance(inport, str): inport = InPort(StringIO.StringIO(inport))
+return expand(read(inport), toplevel=True)
+
+class InPort(object):
+    
+# tokeniser = r"""\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)"""
+   
+tokeniser = "[:space:]*
+                (,@|
+                    [('`,)]|
+                    \"(?:[\\].|[^\\\"])*\"|
+                    ;.*|
+                    [^[:space:]('\"`,;)]*)
+                (.*)"
+tokeniser = "[[:space:]]*\\(,@|[('`,)]|\"(?:[\\].|[^\\\"])*\"|;.*|[^[[:space:]]('\"`,;)]*\\)\\(.*\\)"
+    def __init__(self, file):
+        self.file = file; self.line = ''
+    def next_token(self):
+        "Return the next token, reading new text into line buffer if needed."
+        while True:
+            if self.line == '': self.line = self.file.readline()
+            if self.line == '': return eof_object
+            token, self.line = re.match(InPort.tokenizer, self.line).groups()
+            if token != '' and not token.startswith(';'):
+                return token
+
+
+InPort <- function(infile){
+    # An input port. Retains a line of chars.
+    symbol.patterns <-  c(",@",
+                          "[('`,)]",
+                          "\"(?:[\\].|[^\\\"])*\"",
+                          ";.*",
+                          "[^[:space:]('\"`,;)]*")
+    tokeniser <- sprintf("[[:space:]]*(%s)(.*)", paste(symbol.patterns, collapse = "|"))
+    line <- ""
+    port <- list(
+    next.token = function(){
+        while(1){
+            if(line == "") line <<- readLines(infile, n = 1)
+            if(!length(line)) {
+                return(eof.object)
+            }
+            matcher <- str_match(line, tokeniser)[-1]
+            token <- matcher[1]
+            line <<- matcher[2]
+            if(token != "" & token != ";") return(token)
+        }
+    })
+    structure(port, class="InPort")
+    port
+}
+
+f = "~/github/SchemeR/myfile.lisp"
+infile <- file(f)
+open(infile)
+
+x <- InPort(infile)
+next.token() 
+    
+    
+}
+
+str_match(line, sprintf("(%s)(.*)", paste(patterns, collapse = "|")))
+str_match(line, paste(patterns, collapse ="|"))
+pat = "[[:space:]]*([[:alpha:]])([[:digit:]])"
+
+
+
